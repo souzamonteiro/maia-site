ffa05009c5a7a8e0ae94aa87c7a7b200159f238c
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2023-07-14 20:21:14 +0000

    Changes were made to the text of the paper to meet reviewer requests

--- install_site.sh
+++ install_site.sh
@@ -3,6 +3,8 @@
 echo "Run this script using sudo and into maia-site directory."
 
 #mkdir /var/www/www.maiascript.com/
+#mkdir /var/www/academy.maiascript.com/
+
 cp -rf * /var/www/www.maiascript.com/
 
 chown -R www-data.www-data /var/www/*
