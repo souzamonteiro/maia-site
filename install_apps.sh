fd8c62a2f3095d5b690cd5949670b0fffbe211d5
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2023-10-30 22:33:36 +0000

    Adicionados scripts para iniciar o servidor VNC

--- install_apps.sh
+++ install_apps.sh
@@ -3,6 +3,6 @@
 cp -rf ../cnatool /var/www/www.maiascript.com/
 #cp -rf ../learningblockly /var/www/www.maiascript.com/
 #cp -rf ../maiarecorder /var/www/www.maiascript.com/
-cp -rf ../maiastudio /var/www/www.maiascript.com/
+#cp -rf ../maiastudio /var/www/www.maiascript.com/
 
 chown -R www-data.www-data /var/www/*
