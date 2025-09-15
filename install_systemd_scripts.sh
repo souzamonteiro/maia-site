18d677a6f48793ed9279ab70eef610da1d15644c
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2025-09-15 09:54:39 -0300

    Updated install scipts

--- install_systemd_scripts.sh
+++ install_systemd_scripts.sh
@@ -5,5 +5,12 @@ echo "Run this script using sudo and into maia-site directory."
 cp systemd/system/* /lib/systemd/system/
 
 systemctl daemon-reload
-systemctl enable code-server
-systemctl start code-server
+
+#systemctl enable code-server
+#systemctl start code-server
+
+systemctl enable tightvncserver
+systemctl start tightvncserver
+
+systemctl enable novnc
+systemctl start novnc
